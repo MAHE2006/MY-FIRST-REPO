04fd8e804e81f61aebec27ed8b276a3217ac103e
MAHE2006 <maheswari.al24@krct.ac.in>
2025-04-06 21:28:46 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ a = int(input("Enter A: "))
 b = int(input("Enter B: "))
 sum = a + b
 mul = a * b
-print("The sum of A and B is ",sum)MmM
+print("The sum of A and B is ",sum)
 print("The product of A and B is",mul)
